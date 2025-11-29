72b675bc00f12ac0d7c55e586c2ff64c2eb6e7e1
Oywayten <oywayten@gmail.com>
2025-11-29 20:45:07 +0300

    6. Массивы и коллекции [#189223]

--- src/main/kotlin/vitaliy/grab/Calc.kt
+++ src/main/kotlin/vitaliy/grab/Calc.kt
@@ -61,6 +61,33 @@ fun drawViaRepeat(size: Int, sb: StringBuilder) {
     }
 }
 
+fun defragment(array: Array<String?>) {
+    var pointer1 = 0
+    var pointer2 = 0
+
+    fun swapIfNeed() {
+        if (array[pointer1] == null
+            && array[pointer2] != null
+        ) {
+            val tmp = array[pointer1]
+            array[pointer1] = array[pointer2]
+            array[pointer2] = tmp
+        }
+    }
+
+    while (pointer2 < array.size) {
+        if (pointer2 <= pointer1
+            || array[pointer2] == null
+        ) {
+            pointer2++
+        } else if (array[pointer1] != null) {
+            pointer1++
+        } else {
+            swapIfNeed()
+        }
+    }
+}
+
 fun main() {
     val drawViaFor: String = draw(5, ::drawViaFor)
     println(drawViaFor)
--- src/test/kotlin/vitaliy/grab/CalcTest.kt
+++ src/test/kotlin/vitaliy/grab/CalcTest.kt
@@ -223,4 +223,23 @@ class CalcTest {
         assertThatThrownBy { draw(size, ::drawViaRepeat) }
             .isExactlyInstanceOf(IllegalArgumentException::class.java)
     }
+
+    @ParameterizedTest
+    @MethodSource("getArraysWithNulls")
+    fun whenDefragmentFromStartToEndHasValidOrder(array: Array<String?>, expected: Array<String?>) {
+        defragment(array)
+        assertThat(array).isEqualTo(expected)
+    }
+
+    private fun getArraysWithNulls(): List<Arguments> {
+        return listOf(
+            Arguments.of(arrayOf("test1", null, "test2", null), arrayOf("test1", "test2", null, null)),
+            Arguments.of(arrayOf("test1", null, null, "test2"), arrayOf("test1", "test2", null, null)),
+            Arguments.of(arrayOf(null, null, "test1", "test2"), arrayOf("test1", "test2", null, null)),
+            Arguments.of(arrayOf(null, null, "test1", null), arrayOf("test1", null, null, null)),
+            Arguments.of(arrayOfNulls<String?>(4), arrayOfNulls<String?>(4)),
+            Arguments.of(arrayOf("test1", "test2", "test3", "test4"), arrayOf("test1", "test2", "test3", "test4"))
+        )
+    }
+
 }
