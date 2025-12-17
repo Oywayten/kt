61bf1091753c9dd973bef67ba9918c5e261468d2
Oywayten <oywayten@gmail.com>
2025-12-17 11:41:36 +0300

    5. Apply [#338631]

---
+++ src/main/kotlin/oywayten/HtmlTable.kt
@@ -0,0 +1,26 @@
+package oywayten
+
+class HtmlTable {
+    fun create(row: Int, cell: Int): String {
+        return StringBuilder().apply {
+            append("<table>")
+            appendRowsWithCells(row, cell)
+            append("\n</table>")
+        }.toString()
+    }
+
+    private fun StringBuilder.appendRowsWithCells(row: Int, cell: Int) {
+        repeat(row) {
+            append("\n    <tr>")
+            appendCells(cell)
+            append("\n    </tr>")
+        }
+    }
+
+    private fun StringBuilder.appendCells(cell: Int) {
+        repeat(cell) {
+            append("\n        <td></td>")
+        }
+    }
+
+}
---
+++ src/test/kotlin/oywayten/HtmlTableTest.kt
@@ -0,0 +1,78 @@
+package oywayten
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class HtmlTableTest {
+
+    @ParameterizedTest
+    @MethodSource("whenCreateDataProvider")
+    fun whenCreateIsExpected(row: Int, cell: Int, expected: String) {
+        val table = HtmlTable()
+        val actual = table.create(row, cell)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Suppress("unused")
+    private fun whenCreateDataProvider(): List<Arguments> =
+        listOf(
+            arguments(4, 2, getExpected4x2()),
+            arguments(
+                1, 1, """
+                <table>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </table>
+            """.trimIndent()
+            ),
+            arguments(
+                1, 0, """
+                <table>
+                    <tr>
+                    </tr>
+                </table>
+            """.trimIndent()
+            ),
+            arguments(
+                0, 1, """
+                <table>
+                </table>
+            """.trimIndent()
+            ),
+            arguments(
+                0, 0, """
+                <table>
+                </table>
+            """.trimIndent()
+            ),
+        )
+
+    private fun getExpected4x2(): String = """
+                <table>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            """.trimIndent()
+
+}
+
