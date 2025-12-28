0fc34e62524dbd445f54cc001c0f66f42d3be9fe
Oywayten <oywayten@gmail.com>
2025-12-28 15:37:07 +0300

    DSL

--- src/main/kotlin/oywayten/dsl/Dbcp.kt
+++ src/main/kotlin/oywayten/dsl/Dbcp.kt
@@ -1,6 +1,7 @@
 package oywayten.dsl
 
 import org.apache.commons.dbcp2.BasicDataSource
+import oywayten.dsl.Dbcp.Builder.poolOf
 
 class Dbcp private constructor() : BasicDataSource() {
 
@@ -13,14 +14,33 @@ class Dbcp private constructor() : BasicDataSource() {
         private var maxIdle: Int = 0
         private var maxOpenPreparedStatements: Int = 0
 
-        fun driverClassName(driverClassName: String): Builder = apply { this.driverClassName = driverClassName }
-        fun url(url: String): Builder = apply { this.url = url }
-        fun userName(userName: String): Builder = apply { this.userName = userName }
-        fun password(password: String): Builder = apply { this.password = password }
-        fun minIdle(minIdle: Int): Builder = apply { this.minIdle = minIdle }
-        fun maxIdle(maxIdle: Int): Builder = apply { this.maxIdle = maxIdle }
-        fun maxOpenPreparedStatements(maxOpenStatements: Int): Builder =
-            apply { this.maxOpenPreparedStatements = maxOpenStatements }
+        fun driverClassName(supplier: () -> String) {
+            driverClassName = supplier()
+        }
+
+        fun url(supplier: () -> String) {
+            url = supplier()
+        }
+
+        fun userName(supplier: () -> String) {
+            userName = supplier()
+        }
+
+        fun password(supplier: () -> String) {
+            password = supplier()
+        }
+
+        fun minIdle(supplier: () -> Int) {
+            this.minIdle = supplier()
+        }
+
+        fun maxIdle(supplier: () -> Int) {
+            this.maxIdle = supplier()
+        }
+
+        fun maxOpenPreparedStatements(supplier: () -> Int) {
+            this.maxOpenPreparedStatements = supplier()
+        }
 
         fun build(): Dbcp = Dbcp().apply {
             driverClassName = Dbcp.driverClassName
@@ -31,7 +51,11 @@ class Dbcp private constructor() : BasicDataSource() {
             maxIdle = Dbcp.maxIdle
             maxOpenPreparedStatements = Dbcp.maxOpenPreparedStatements
         }
+
+        fun poolOf(body: Builder.() -> Dbcp): Dbcp =
+            body()
     }
+
 
     override fun toString(): String {
         return buildString {
@@ -50,18 +74,20 @@ class Dbcp private constructor() : BasicDataSource() {
 
 }
 
+
 @Suppress("MagicNumber")
 fun main() {
-    val pool = Dbcp
-        .driverClassName("org.postgres.Driver")
-        .url("localhost")
-        .userName("postgres")
-        .password("password")
-        .minIdle(5)
-        .maxIdle(10)
-        .maxOpenPreparedStatements(100)
-        .build()
+    val pool: Dbcp = poolOf {
+        driverClassName { "org.postgres.Driver" }
+        url { "localhost" }
+        userName { "postgres" }
+        password { "password" }
+        minIdle { 5 }
+        maxIdle { 10 }
+        maxOpenPreparedStatements { 100 }
+        build()
+    }
+
     println(pool)
 }
-
 
